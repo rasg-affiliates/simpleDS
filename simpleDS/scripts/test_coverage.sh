045bfb1dde63deba218f8e967c66026249166d20
Matthew Kolopanis <matthew.kolopanis@gmail.com>
2018-07-24 09:30:10 -0700

    removed the test script in wrong place

--- simpleDS/scripts/test_coverage.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-cd $DIR/..
-
-python setup.py install
-
-cd simpleDS/tests
-nosetests --with-coverage --cover-erase --cover-package=simpleDS --cover-html "$@"
