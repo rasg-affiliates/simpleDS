dc72b118cae17005946c25928a4a5935f4f97e7c
Matthew Kolopanis <matthew.kolopanis@gmail.com>
2018-07-24 09:28:37 -0700

    __init__ files for repo and for tests

---
+++ simpleDS/__init__.py
@@ -0,0 +1,9 @@
+"""Init file for simpleDS."""
+
+
+from __future__ import print_function, absolute_import
+
+from . import version
+from .utils import *
+
+__version__ = version.version
---
+++ simpleDS/tests/__init__.py
@@ -0,0 +1,10 @@
+"""Setup testing environment, define useful testing functions.
+
+"""
+from __future__ import absolute_import, division, print_function
+
+import os
+import warnings
+import sys
+import numpy as np
+from simpleDS.data import DATA_PATH
