41c837f01d7874822068df82662a7a495fd0a83a
Matthew Kolopanis <matthew.kolopanis@gmail.com>
2018-07-24 16:44:31 -0700

    Added function which calculates delay FT, tests and cross-multiplication

---
+++ simpleDS/delay_spectrum.py
@@ -0,0 +1,68 @@
+"""Calculate Delay Spectrum from pyuvdata object."""
+from __future__ import print_function
+
+import os
+import sys
+import numpy as np
+from pyuvdata import UVData
+from builtins import range, zip
+import astropy.units as units
+from astropy.units import Quantity
+from astropy import constants as const
+from scipy.signal import windows
+
+
+def jy_to_mk(freqs):
+    """Calculate the Jy to mK conversion lambda^2/(2 * K_boltzman)."""
+    assert(isinstance(freqs, Quantity))
+    jy2t = const.c.to('m/s')**2 / (2 * freqs.to('1/s')**2
+                                   * const.k_B)
+    return jy2t.to('mK/Jy')
+
+
+def delay_transform(data_1_array, data_2_array=None,
+                    chans=None, delta_f=1. * units.Hz,
+                    window=windows.blackmanharris):
+    """Perform the Dealy transform over specified channel rangeself.
+
+    Perform the FFT over frequency using the specified window function
+    and cross multiplies all baselines in the data array.
+
+    Arguments:
+        data_1_array : (Nbls, Nfreqs, Ntimes) array from utils.get_data_array
+        data_2_array : same type as data_1_array if take cross-multiplication
+                       Defaults to copying data_1_array
+        chans : list channel range start and stop (eg: [100,300])
+        window : Window function used in delay transform.
+                 Default is scipy.signal.windows.blackmanharris
+    Returns:
+        delay_power: (Nbls, Nbls, Nfreqs, Ntimes) array
+                     Cross multplies all baselines in data_1_array
+    """
+    if isinstance(data_1_array, Quantity):
+        unit = data_1_array.unit
+    else:
+        unit = 1.
+
+    if data_2_array is None:
+        data_2_array = data_1_array.copy()
+
+    nfreqs = data_1_array.shape[1]
+    win = window(nfreqs).reshape(1, nfreqs, 1)
+
+    # Fourier Transforms should have a delta_f term multiplied
+    # This is the proper normalization of the FT but is not
+    # accounted for in an fft.
+    delay_1_array = np.fft.fft(data_1_array * win, axis=1)
+    delay_1_array = np.fft.fftshift(delay_1_array, axes=1)
+    delay_1_array = delay_1_array * delta_f.to('1/s') * unit
+
+    delay_2_array = np.fft.fft(data_2_array * win, axis=1)
+    delay_2_array = np.fft.fftshift(delay_2_array, axes=1)
+    delay_2_array = delay_2_array *delta_f.to('1/s') * unit
+
+    # Using slicing for cross-multiplication should be quick.
+    delay_power = (delay_1_array[None, :, :].conj() *
+                   delay_2_array[:, None, :, :])
+
+    return delay_power
---
+++ simpleDS/tests/test_delay_spectrum.py
@@ -0,0 +1,55 @@
+"""Test Delay Spectrum calculations."""
+from __future__ import print_function
+
+import os
+import sys
+import numpy as np
+import nose.tools as nt
+import pyuvdata
+from pyuvdata import UVData, utils as uvutils
+from simpleDS import delay_spectrum as dspec
+from simpleDS.data import DATA_PATH
+from builtins import range, zip
+from astropy import constants as const
+from astropy import units
+from scipy.signal import windows
+
+
+def test_jy_to_mk_value():
+    """Test the Jy to mK conversion factor."""
+    test_fq = np.array([.1])*units.GHz
+    jy_to_mk = dspec.jy_to_mk(test_fq)
+    test_conversion = const.c**2 / (2 * test_fq.to('1/s')**2 * const.k_B)
+    test_conversion = test_conversion.to('mK/Jy')
+    nt.assert_true(np.allclose(test_conversion.value, jy_to_mk.value))
+
+
+def test_jy_to_mk_units():
+    """Test the Jy to mK conversion factor."""
+    test_fq = np.array([.1])*units.GHz
+    jy_to_mk = dspec.jy_to_mk(test_fq)
+    test_conversion = const.c**2 / (2 * test_fq.to('1/s')**2 * const.k_B)
+    test_conversion = test_conversion.to('mK/Jy')
+    nt.assert_equal(test_conversion.unit.to_string(),
+                    jy_to_mk.unit.to_string())
+
+
+def test_delay_transform():
+    """Test the delay transform and cross-multiplication function."""
+    fake_data = np.zeros((1, 21, 13))
+    fake_data[0, 11, 7] += 1
+    fake_corr = dspec.delay_transform(fake_data, window=windows.boxcar)
+    test_corr = np.fft.fft(fake_data, axis=1)
+    test_corr = np.fft.fftshift(test_corr, axes=1)
+    test_corr = test_corr[None, ...].conj() * test_corr[:, None, ...]
+    fake_corr = fake_corr.value
+    nt.assert_true(np.allclose(test_corr, fake_corr))
+
+
+def test_units_delay_transform():
+    """Test units are returned squared from delay_transform."""
+    fake_data = np.zeros((1, 21, 13)) * units.m
+    fake_data[0, 11, 7] += 1 * units.m
+    fake_corr = dspec.delay_transform(fake_data, window=windows.boxcar)
+    test_units = (units.m*units.Hz)**2
+    nt.assert_equal(test_units, fake_corr.unit)
