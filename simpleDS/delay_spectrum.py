19aa32c738d1b165b22d874e0899a8a23e2c3e76
Matthew Kolopanis <matthew.kolopanis@gmail.com>
2018-07-25 12:19:37 -0700

    catch if data is not same size in delay_transform, handles pol dimension

--- simpleDS/delay_spectrum.py
+++ simpleDS/delay_spectrum.py
@@ -29,17 +29,20 @@ def delay_transform(data_1_array, data_2_array=None,
     and cross multiplies all baselines in the data array.
 
     Arguments:
-        data_1_array : (Nbls, Nfreqs, Ntimes) array from utils.get_data_array
+        data_1_array : (Nbls, Ntimes, Nfreqs) array from utils.get_data_array
+                        Can also have shape (Npols, Nbls, Nfreqs, Ntimes)
         data_2_array : same type as data_1_array if take cross-multiplication
-                       Defaults to copying data_1_array
+                       Defaults to copying data_1_array for auto-correlation
         delta_f: The difference between frequency channels in the data.
                  This is used to properly normalize the Fourier Transform.
                  Must be an astropy Quantity object
         window : Window function used in delay transform.
                  Default is scipy.signal.windows.blackmanharris
     Returns:
-        delay_power: (Nbls, Nbls, Nfreqs, Ntimes) array
+        delay_power: (Nbls, Nbls, Ntimes, Nfreqs) array
                      Cross multplies all baselines in data_1_array
+                     if pols are present returns
+                     (Npols, Nbls, Nbls, Ntimes, Nfreqs)
     """
     if isinstance(data_1_array, Quantity):
         unit = data_1_array.unit
@@ -53,22 +56,31 @@ def delay_transform(data_1_array, data_2_array=None,
     if data_2_array is None:
         data_2_array = data_1_array.copy()
 
-    nfreqs = data_1_array.shape[1]
-    win = window(nfreqs).reshape(1, nfreqs, 1)
+    if not data_2_array.shape == data_1_array.shape:
+        raise ValueError('data_1_array and data_2_array must have same shape, '
+                         'but data_1_array has shape {d1_s} and '
+                         'data_2_array has shape {d2_s}'
+                         .format(d1_s=data_1_array.shape,
+                                 d2_s=data_2_array.shape))
+    nfreqs = data_1_array.shape[-1]
+    win = window(nfreqs).reshape(1, nfreqs)
 
     # Fourier Transforms should have a delta_f term multiplied
     # This is the proper normalization of the FT but is not
     # accounted for in an fft.
-    delay_1_array = np.fft.fft(data_1_array * win, axis=1)
-    delay_1_array = np.fft.fftshift(delay_1_array, axes=1)
+    delay_1_array = np.fft.fft(data_1_array * win, axis=-1)
+    delay_1_array = np.fft.fftshift(delay_1_array, axes=-1)
     delay_1_array = delay_1_array * delta_f.to('1/s') * unit
 
-    delay_2_array = np.fft.fft(data_2_array * win, axis=1)
-    delay_2_array = np.fft.fftshift(delay_2_array, axes=1)
-    delay_2_array = delay_2_array *delta_f.to('1/s') * unit
+    delay_2_array = np.fft.fft(data_2_array * win, axis=-1)
+    delay_2_array = np.fft.fftshift(delay_2_array, axes=-1)
+    delay_2_array = delay_2_array * delta_f.to('1/s') * unit
 
     # Using slicing for cross-multiplication should be quick.
-    delay_power = (delay_1_array[None, :, :].conj() *
-                   delay_2_array[:, None, :, :])
-
+    if len(data_1_array.shape) == 3:
+        delay_power = (delay_1_array[None, :, :].conj() *
+                       delay_2_array[:, None, :, :])
+    else:
+        delay_power = (delay_1_array[:, None, :, :].conj() *
+                       delay_2_array[:, :, None, :, :])
     return delay_power
--- simpleDS/tests/test_delay_spectrum.py
+++ simpleDS/tests/test_delay_spectrum.py
@@ -34,22 +34,49 @@ def test_jy_to_mk_units():
                     jy_to_mk.unit.to_string())
 
 
+def test_data_2_wrong_shape():
+    """Test Exception is raised if shapes do not match."""
+    fake_data_1 = np.zeros((1, 13, 21))
+    fake_data_2 = np.zeros((2, 13, 21))
+    nt.assert_raises(ValueError, dspec.delay_transform,
+                     fake_data_1, fake_data_2)
+
+
 def test_delay_transform():
     """Test the delay transform and cross-multiplication function."""
-    fake_data = np.zeros((1, 21, 13))
-    fake_data[0, 11, 7] += 1
+    fake_data = np.zeros((1, 13, 21))
+    fake_data[0, 7, 11] += 1
     fake_corr = dspec.delay_transform(fake_data, window=windows.boxcar)
-    test_corr = np.fft.fft(fake_data, axis=1)
-    test_corr = np.fft.fftshift(test_corr, axes=1)
+    test_corr = np.fft.fft(fake_data, axis=-1)
+    test_corr = np.fft.fftshift(test_corr, axes=-1)
     test_corr = test_corr[None, ...].conj() * test_corr[:, None, ...]
     fake_corr = fake_corr.value
     nt.assert_true(np.allclose(test_corr, fake_corr))
 
 
+def test_delay_with_pols():
+    """Test delay transform is correct shape when polarizations are present."""
+    fake_data = np.zeros((3, 2, 13, 31))
+    fake_data[:, 0, 7, 11] += 1.
+    fake_corr = dspec.delay_transform(fake_data, window=windows.boxcar)
+    nt.assert_equal((3, 2, 2, 13, 31), fake_corr.shape)
+
+
+def test_delay_vals_with_pols():
+    """Test values in delay_transform when pols present."""
+    fake_data = np.zeros((3, 2, 13, 31))
+    fake_data[:, 0, 7, 11] += 1.
+    fake_corr = dspec.delay_transform(fake_data, window=windows.boxcar)
+    test_corr = np.fft.fft(fake_data, axis=-1)
+    test_corr = np.fft.fftshift(test_corr, axes=-1)
+    test_corr = test_corr[:, None, ...].conj() * test_corr[:, :, None, ...]
+    fake_corr = fake_corr.value
+    nt.assert_true(np.allclose(test_corr, fake_corr))
+
 def test_units_delay_transform():
     """Test units are returned squared from delay_transform."""
-    fake_data = np.zeros((1, 21, 13)) * units.m
-    fake_data[0, 11, 7] += 1 * units.m
+    fake_data = np.zeros((1, 13, 21)) * units.m
+    fake_data[0, 7, 11] += 1 * units.m
     fake_corr = dspec.delay_transform(fake_data, window=windows.boxcar)
     test_units = (units.m*units.Hz)**2
     nt.assert_equal(test_units, fake_corr.unit)
@@ -57,6 +84,6 @@ def test_units_delay_transform():
 
 def test_delta_f_unitless():
     """Test delta_f is unitless raises exception."""
-    fake_data = np.zeros((1, 21, 13)) * units.m
-    fake_data[0, 11, 7] += 1 * units.m
+    fake_data = np.zeros((1, 13, 21)) * units.m
+    fake_data[0, 7, 11] += 1 * units.m
     nt.assert_raises(ValueError, dspec.delay_transform, fake_data, delta_f=2.)
