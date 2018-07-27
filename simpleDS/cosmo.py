ac0e811d063f26b018d9e077efc998567f3d138a
Matthew Kolopanis <matthew.kolopanis@gmail.com>
2018-07-27 11:37:47 -0700

    added cosmology conversions

---
+++ simpleDS/cosmo.py
@@ -0,0 +1,76 @@
+"""Relevent Cosmology units and transforms for Power Spectrum estimation.
+
+All cosmological calculations and converions follow from Liu et al 2014a
+Phys. Rev. D 90, 023018 or 	arXiv:1404.2596
+"""
+
+import os
+import sys
+import numpy as np
+from scipy import integrate
+from astropy import constants as const
+from astropy import units
+from astropy.units import Quantity
+from astropy.cosmology import WMAP9
+
+# the emission frequency of 21m photons
+f21 = 1420405751.7667 * units.Hz
+
+# Using WMAP 9-year cosmology as the default
+# move in the the little-h unit frame by setting H0=100
+little_h_cosmo = WMAP9.clone(name='WMAP9 h-units', H0=100)
+
+
+def calc_z(freq):
+    """Calculate the redshift from a given frequency or frequncies."""
+    if not isinstance(freq, Quantity):
+        raise ValueError("Input freq must be an astropy Quantity. "
+                         "value was: {}".format(freq))
+    return (f21/freq).si.value - 1
+
+
+def u2kperp(u, z, cosmo=little_h_cosmo):
+    """Convert baseline length u to k_perpendical."""
+    return 2 * np.pi * u / cosmo.comoving_distance(z)
+
+
+def kperp2u(kperp, z, cosmo=little_h_cosmo):
+    """Convert comsological k to baseline length u."""
+    if not isinstance(kperp, Quantity):
+        raise ValueError('input kperp must be an astropy Quantity object. '
+                         'value was: {0}'.format(kperp))
+    return kperp * cosmo.comoving_distance(z) / (2 * np.pi)
+
+
+def eta2kparr(eta, z, cosmo=little_h_cosmo):
+    """Conver the delay eta to comslogical comoving Mpc along line of sight."""
+    if not isinstance(eta, Quantity):
+        raise ValueError('input eta must be an astropy Quantity object. '
+                         'value was: {0}'.format(eta))
+    return (eta * (2 * np.pi * cosmo.H0 * f21 * cosmo.efunc(z)) /
+            (const.c * (1 + z)**2)).to('1/Mpc')
+
+
+def kparr2eta(kparr, z, cosmo=little_h_cosmo):
+    """Convert k_par (h/Mpc along line of sight) to delay eta."""
+    if not isinstance(kparr, Quantity):
+        raise ValueError('input kparr must be an astropy Quantity object. '
+                         'value was: {0}'.format(kparr))
+    return (kparr * const.c * (1 + z)**2
+            / (2 * np.pi * cosmo.H0 * f21 * cosmo.efunc(z))).to('s')
+
+
+def X2Y(z, cosmo=little_h_cosmo):
+    """Convert units from interfometric delay power to comsological power.
+
+    Converts power spectrum from interfometric units of eta, u to
+    cosmological k_par, k_perp.
+
+    Arguments:
+        z: redshift for cosmological conversion
+    Returns:
+        X2Y: The power spectrum unit conversion factor
+    """
+    X2Y = const.c * (1 + z)**2 * cosmo.comoving_distance(z)**2
+    X2Y /= cosmo.H0 * f21 * cosmo.efunc(z)
+    return X2Y.to('Mpc^3*s')
---
+++ simpleDS/tests/test_cosmo.py
@@ -0,0 +1,158 @@
+"""Tests for cosmo.property
+
+Relevent Cosmology units and transforms for Power Spectrum estimation.
+
+All cosmological calculations and converions follow from Liu et al 2014a
+Phys. Rev. D 90, 023018 or 	arXiv:1404.2596
+"""
+from __future__ import print_function
+
+import os
+import sys
+import numpy as np
+import nose.tools as nt
+from scipy import integrate
+from astropy import constants as const
+from astropy import units
+from astropy.units import Quantity
+from astropy.cosmology import WMAP9
+from simpleDS.data import DATA_PATH
+from simpleDS import cosmo
+# the emission frequency of 21m photons
+f21 = 1420405751.7667 * units.Hz
+
+# Using WMAP 9-year cosmology as the default
+# move in the the little-h unit frame by setting H0=100
+little_h_cosmo = WMAP9.clone(name='WMAP9 h-units', H0=100)
+
+
+def test_calc_z_freq_unitless():
+    """Test redshift calculation fails for non-quantity freqs."""
+    test_freq = .15e9
+    nt.assert_raises(ValueError, cosmo.calc_z, test_freq)
+
+
+def test_calc_z_value():
+    """Test redshift calculation value."""
+    test_freq = .15 * units.GHz
+    test_z = f21 / test_freq - 1
+    nt.assert_true(np.isclose(test_z, cosmo.calc_z(test_freq)))
+
+
+def test_u2kperp_unit():
+    """Test units of u2kperp."""
+    test_z = 7.6363125
+    test_u = 10
+    test_kperp = cosmo.u2kperp(test_u, test_z)
+    nt.assert_equal(1./units.Mpc, test_kperp.unit)
+
+
+def test_u2kperp_val():
+    """Test value of kperp calculation."""
+    test_z = 7.6363125
+    test_u = 10
+    test_kperp = cosmo.u2kperp(test_u, test_z)
+    nt.assert_true(np.isclose(.01, test_kperp.value))
+
+
+def test_kperp2u_error():
+    """Test kperp must be a Quantity."""
+    test_kperp = .01
+    test_z = 7.6363125
+    nt.assert_raises(ValueError, cosmo.kperp2u, test_kperp, test_z)
+
+
+def test_kperp2u_unit():
+    """Test kperp2u units."""
+    test_kperp = .01 * 1./units.Mpc
+    test_z = 7.6363125
+    test_u = cosmo.kperp2u(test_kperp, test_z)
+    nt.assert_equal(test_u.unit.bases, [])
+
+
+def test_kperp2u_value():
+    """Test kperp2u value."""
+    test_kperp = .01 * 1./units.Mpc
+    test_z = 7.6363125
+    test_u = cosmo.kperp2u(test_kperp, test_z)
+    nt.assert_true(np.isclose(10, test_u.value))
+
+
+def test_kperp2u2kperp_equal():
+    """Test that kperp2u and u2kperp undo each other."""
+    test_kperp = .01 / units.Mpc
+    test_z = 7.6363125
+    test_u = cosmo.kperp2u(test_kperp, test_z)
+    test_u2kperp = cosmo.u2kperp(test_u, test_z)
+    nt.assert_equal(test_kperp, test_u2kperp)
+
+
+def test_eta2kparr_error():
+    """Test eta must be Quantity."""
+    test_eta = 200 * 1e-9
+    test_z = 9.19508
+    nt.assert_raises(ValueError, cosmo.eta2kparr, test_eta, test_z)
+
+
+def test_eta2kparr_val():
+    """Test eta must be Quantity."""
+    test_eta = 200 * 1e-9 * units.s
+    test_z = 9.19508
+    test_kparr = cosmo.eta2kparr(test_eta, test_z)
+    nt.assert_true(np.isclose(.1, test_kparr.value))
+
+
+def test_eta2kparr_unit():
+    """Test eta must be Quantity."""
+    test_eta = 200 * 1e-9 * units.s
+    test_z = 9.19508
+    test_kparr = cosmo.eta2kparr(test_eta, test_z)
+    nt.assert_equal(1./units.Mpc, test_kparr.unit)
+
+
+def test_kparr2eta_error():
+    """Test kparr2eta errors for non quantity objects."""
+    test_kparr = .1
+    test_z = 9.19508
+    nt.assert_raises(ValueError, cosmo.kparr2eta, test_kparr, test_z)
+
+
+def test_kparr2eta_val():
+    """Test kparr2eta value matches."""
+    test_kparr = .1 / units.Mpc
+    test_z = 9.19508
+    test_eta = cosmo.kparr2eta(test_kparr, test_z)
+    nt.assert_true(np.isclose(200, test_eta.value * 1e9))
+
+
+def test_kparr2eta_unit():
+    """Test kparr2eta unit matches."""
+    test_kparr = .1 / units.Mpc
+    test_z = 9.19508
+    test_eta = cosmo.kparr2eta(test_kparr, test_z)
+    nt.assert_equal(units.s, test_eta.unit)
+
+
+def test_kparr2eta_eta2kparr():
+    """Test that kparr2eta and eta2kparr undo each other."""
+    test_kparr = .1 / units.Mpc
+    test_z = 9.19508
+    test_eta = cosmo.kparr2eta(test_kparr, test_z)
+    test_eta2kparr = cosmo.eta2kparr(test_eta, test_z)
+    nt.assert_true(np.isclose(test_kparr.value, test_eta2kparr.value))
+
+
+def test_X2Y_unit():
+    """Test unit on X2Y are 1/(Hz/Mpc)^3 or Mpc^3 * s."""
+    test_z = 7
+    test_x2y = cosmo.X2Y(test_z)
+    nt.assert_equal(units.Mpc**3 * units.s, test_x2y.unit)
+
+
+def tests_X2Y_val():
+    """Test value of X2Y."""
+    test_z = 7
+    test_x2y = cosmo.X2Y(test_z)
+    compare_x2y = (2*np.pi)**3 / (cosmo.u2kperp(1, test_z)**2
+                                  * cosmo.eta2kparr(1 * units.s, test_z))
+    nt.assert_true(np.isclose(compare_x2y.value, test_x2y.value))
