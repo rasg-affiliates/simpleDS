de03cc9df65f6708a62c58ea97a3ac407102c340
Matthew Kolopanis <Matthew.Kolopanis@gmail.com>
2019-05-10 13:29:11 -0700

    update version to pytest

--- simpleDS/tests/test_version.py
+++ simpleDS/tests/test_version.py
@@ -4,7 +4,7 @@
 """Tests for version.py."""
 from __future__ import absolute_import, division, print_function
 
-import nose.tools as nt
+import pytest
 import sys
 import os
 import six
@@ -47,7 +47,7 @@ def test_get_gitinfo_file():
     else:
         file_info = simpleDS.version._get_gitinfo_file()
 
-    nt.assert_equal(file_info, test_file_info)
+    assert file_info == test_file_info
 
 
 def test_construct_version_info():
@@ -113,7 +113,7 @@ def test_construct_version_info():
                          'git_hash': git_hash, 'git_description': git_description,
                          'git_branch': git_branch}
 
-    nt.assert_equal(simpleDS.version.construct_version_info(), test_version_info)
+    assert simpleDS.version.construct_version_info() == test_version_info
 
 
 def test_main():
@@ -126,11 +126,11 @@ def test_main():
         sys.stdout = out
         simpleDS.version.main()
         output = out.getvalue()
-        nt.assert_equal(output, 'Version = {v}\ngit origin = {o}\n'
-                        'git branch = {b}\ngit description = {d}\n'
-                        .format(v=version_info['version'],
-                                o=version_info['git_origin'],
-                                b=version_info['git_branch'],
-                                d=version_info['git_description']))
+        assert (output == ('Version = {v}\ngit origin = {o}\n'
+                           'git branch = {b}\ngit description = {d}\n'
+                           .format(v=version_info['version'],
+                                   o=version_info['git_origin'],
+                                   b=version_info['git_branch'],
+                                   d=version_info['git_description'])))
     finally:
         sys.stdout = saved_stdout
