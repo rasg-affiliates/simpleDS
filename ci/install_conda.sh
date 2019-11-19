76cc615c2e1f72e4e8f2544ac1dc61c7de96e2da
Matthew Kolopanis <Matthew.Kolopanis@gmail.com>
2019-11-19 14:04:06 -0700

    use source activate

--- ci/install_conda.sh
+++ ci/install_conda.sh
@@ -16,7 +16,7 @@ conda update -q conda
 conda info -a
 conda create --name=${ENV_NAME}  python=$PYTHON --quiet
 conda env update -f ci/${ENV_NAME}.yml
-conda activate ${ENV_NAME}
+source activate ${ENV_NAME}
 conda list -n ${ENV_NAME}
 # check that the python version matches the desired one; exit immediately if not
 PYVER=`python -c "from __future__ import print_function; import sys; print('{:d}.{:d}'.format(sys.version_info.major, sys.version_info.minor))"`
