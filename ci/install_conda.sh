f68a70140936545b9c287e4e40b0fc11ced5bf2d
Matthew Kolopanis <Matthew.Kolopanis@gmail.com>
2019-11-19 15:01:14 -0700

    make sure curl is installed in env

--- ci/install_conda.sh
+++ ci/install_conda.sh
@@ -5,10 +5,10 @@ set -xe
 if [[ ! $OS == 'macos-latest' ]]; then
   if [ ! -z "$WITH_SUDO" ]; then
     sudo apt-get update
-    sudo apt-get install -y gcc g++
+    sudo apt-get install -y gcc g++ curl
   else
     apt-get update
-    apt-get install -y gcc g++
+    apt-get install -y gcc g++ curl
   fi
 fi
 conda config --set always_yes yes --set changeps1 no
