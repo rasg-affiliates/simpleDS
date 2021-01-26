372a34a12a289ad71267e5835066b3afc4b4aa59
Matthew Kolopanis <Matthew.Kolopanis@gmail.com>
2021-01-26 14:20:30 -0700

    switch to pytest tmp_path fixture, make setup_and_teardown pass

--- simpleDS/tests/conftest.py
+++ simpleDS/tests/conftest.py
@@ -3,22 +3,10 @@
 # Licensed under the 3-clause BSD License
 
 """Testing environment setup and teardown for pytest."""
-import os
-import shutil
-
 import pytest
-from simpleDS.data import DATA_PATH
 
 
 @pytest.fixture(autouse=True, scope="session")
 def setup_and_teardown_package():
     """Make data/test directory to put test output files in."""
-    testdir = os.path.join(DATA_PATH, "test_data/")
-    pytest.testdir = testdir
-    if not os.path.exists(testdir):
-        print("making test directory")
-        os.mkdir(testdir)
-
     yield
-
-    shutil.rmtree(testdir)
