63c8a50d5d73f561a7609d2412bea716e1ee1ea0
Matthew Kolopanis <Matthew.Kolopanis@gmail.com>
2022-06-06 14:43:17 -0400

    move copy-pasta setuptools versioning to main init

--- simpleDS/__init__.py
+++ simpleDS/__init__.py
@@ -2,15 +2,25 @@
 # Copyright (c) 2018 rasg-affiliates
 # Licensed under the 3-clause BSD License
 """Init file for simpleDS."""
-
+from pathlib import Path
+from setuptools_scm import get_version
 from pkg_resources import get_distribution, DistributionNotFound
 
-# Set the version automatically from the package details.
-try:
-    __version__ = get_distribution(__name__).version
-except DistributionNotFound:  # pragma: nocover
-    # package is not installed
-    pass
+from .branch_scheme import branch_scheme
 
 from .utils import *  # noqa
 from .delay_spectrum import *  # noqa
+
+try:  # pragma: nocover
+    # get accurate version for developer installs
+    version_str = get_version(Path(__file__).parent.parent, local_scheme=branch_scheme)
+
+    __version__ = version_str
+
+except (LookupError, ImportError):
+    try:
+        # Set the version automatically from the package details.
+        __version__ = get_distribution(__name__).version
+    except DistributionNotFound:  # pragma: nocover
+        # package is not installed
+        pass
